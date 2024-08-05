a22af7c6a3c449718ada3a3ec095d3a9ce3da6e2
Pratyush Sharma <94819127+Pratyush1403@users.noreply.github.com>
2024-08-05 08:27:11 +0530

    Update Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './Auth.css'; // Import the CSS file
+import { URL } from './config'
 
 const Auth = ({ setUser }) => {
   const [isSignUp, setIsSignUp] = useState(false);
